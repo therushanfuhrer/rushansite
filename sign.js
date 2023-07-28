7a76a602b6fdcdef2060ae03719329c2a4db52ac
therushanfuhrer <140135149+therushanfuhrer@users.noreply.github.com>
2023-07-28 16:01:57 -0700

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,1 +1,7 @@
-
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+//drawClock();
+setInterval(drawClock, 1000);
